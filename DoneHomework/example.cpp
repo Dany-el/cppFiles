21272dcc7f893d9a80fdb1f8e7c4ed838e0a76a4
Daniel <Ablo_eh91@student.itstep.org>
2021-12-18 13:46:08 +0200

    Создал новую папку для сделанных дз

---
+++ DoneHomework/example.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file example.cpp
+ * @author Daniel Yablonskyi (daniilyablonskiy88@gmail.com)
+ * @brief Here will be test features
+ * @version 0.1
+ * @date 2021-12-18
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <iostream>
+
+using std::cout,std::cin,std::string;
+
+int main(){
+
+    cout<<"hello world\n";
+
+    return 0;
+}
