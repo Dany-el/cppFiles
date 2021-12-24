cdea33c6c224e54ec119452d33560579001aafd5
Dany-el <daniilyablonskiy88@gmail.com>
2021-12-24 11:36:51 +0200

    Создал библиотеку. Прокомпелировал

--- TestFiles/ex.cpp
+++ TestFiles/ex.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <cstring>
 #include "header.h"
+#include "file.h"
 
 using std::string;
 using std::cout;
 using std::cin;
 
+void function(){
+	cout<<"Hello world\n";
+}
+
 int main(){
 	f exp;
 	exp.do_sth();
+	function();
+	Function tmp;
+	tmp.do_sth_2();
+	
   	return 0;
 }
---
+++ TestFiles/file.cpp
@@ -0,0 +1,6 @@
+#include "file.h"
+#include <iostream>
+
+void Function::do_sth_2(){
+	std::cout<<"\n2 + 2 = "<< 2 + 2 << '\n';
+}
