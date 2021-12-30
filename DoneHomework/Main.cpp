91ff578ff702c6cabf1cd5d6f171d27bfcd0d5ba
Dany-el <daniilyablonskiy88@gmail.com>
2021-12-30 14:23:15 +0200

    Добавил комментарии к методам

--- DoneHomework/Main.cpp
+++ DoneHomework/Main.cpp
@@ -6,25 +6,36 @@
 using std::cout;
 using std::cin;
 
+/*
+ * Периметр комнаты
+
+ @param p_width  - ширина комнаты
+ @param p_length - длина комнаты
+
+ @return Периметр комнаты
+*/
 int Perimeter(int p_width,int p_length){
 	return 2*(p_width+p_length);
 }
 
+/*
+ * Площадь комнаты
+
+@param perimeter - периметр комнаты
+@param p_height  - высота комнаты
+
+@return Площадь комнаты
+*/
 int Area(int perimeter,int p_heigth){
 	int area = perimeter * p_heigth;
 	return area;
 }
 
 
-
 int main()
 {
 	Apartment* obj1 = new Apartment();
 	obj1->Input();
-	//Room* obj2 = new Room();
-	//obj2->Input();
-	// cout << "\nArea of the room: " << 
-	// 		Area(Perimeter(obj1->getWidth(),obj1->getLength()),
-	// 		obj1->getHeight()) << '\n';
+	
 	return 0;
 }
