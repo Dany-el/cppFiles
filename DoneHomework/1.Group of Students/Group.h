0c6a6bd8ec1d5a083e25374084f6fa905df60923
Daniel <96320286+Dany-el@users.noreply.github.com>
2022-01-25 18:52:32 +0200

    Add const method(accessor)

--- DoneHomework/1.Group of Students/Group.h
+++ DoneHomework/1.Group of Students/Group.h
@@ -11,9 +11,9 @@ private:
 	// Subjects(array)
 	Subject* subject_array;
 public:
-	string* getName();
-	Student* getStudent_array();
-	Subject* getSubject_array();
+	string* getName() const;
+	Student* getStudent_array() const;
+	Subject* getSubject_array() const;
 
 	void setName(const string p_name);
 	void setStudent(const size_t student_amount);
@@ -22,7 +22,7 @@ public:
 	Group();
 	Group(const string p_name,const size_t student_amount,const size_t subject_amount);
 
-	void Print(size_t** grades_array,const size_t student_amount,const size_t subject_amount);
+	void Print(const size_t** grades_array,const size_t student_amount,const size_t subject_amount) const;
 	void Input();
 
 	void Max_grade(size_t** grades_array,const size_t student_amount,const size_t subject_amount);
