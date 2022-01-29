79a8fd6b33815834db830185b79ee2f91ae35232
Dany-el <daniilyablonskiy88@gmail.com>
2022-01-29 23:17:45 +0200

    Add Class Date

---
+++ DoneHomework/4. Date/Date.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class Date{
+    // Day
+    unsigned int day;
+    // Month
+    unsigned int month;
+    // Year
+    unsigned int year;
+    // Methods for checking
+    // for Day
+    void CheckInputDay();
+    void CheckOneSevenMonth();
+    void CheckEightTwelveMonth();
+    // for Month
+    void CheckInputMonth();
+    // for Year
+    void CheckInputYear();
+    // Converter of date -> days
+    unsigned int ConvertDateIntoDays(const Date p_date);
+    // Converter of days -> date
+    Date ConvertDaysIntoDate(unsigned int full_days);
+public:
+    inline unsigned int getDay() const;
+    inline unsigned int getMonth() const;
+    inline unsigned int getYear() const;
+
+    inline void setDay(const unsigned int p_day);
+    inline void setMonth(const unsigned int p_month);
+    inline void setYear(const unsigned int p_year);
+
+    Date();
+    Date(const unsigned int p_day,const unsigned int p_month, const unsigned int p_year);
+
+    void Input();
+    void Print();
+    unsigned int operator - (const Date second_date);
+    Date operator + (const unsigned int p_days);
+
+    ~Date();
+};
