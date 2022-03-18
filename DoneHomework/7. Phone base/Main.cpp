05c756458da7452dc1ba7824b2cc42b3134af651
Dany-el <daniilyablonskiy88@gmail.com>
2022-03-18 20:48:19 +0200

    poligon

--- DoneHomework/7. Phone base/Main.cpp
+++ DoneHomework/7. Phone base/Main.cpp
@@ -3,6 +3,8 @@
 
 int main()
 {
+    system("sudo rm -r Base.txt");
+
     Base tmp2;
     tmp2.add_contact();
     tmp2.add_contact();
@@ -12,8 +14,12 @@ int main()
     // tmp2.find_contact_by_pnumber();
     // tmp2.find_contact_by_surname();
 
-    tmp2 = tmp2.print_contact_by_alp();
-    tmp2.print_contact();
+    // tmp2 = tmp2.print_contact_by_alp();
+    // tmp2.print_contact();
+
+    // tmp2.write_file();
+
+    tmp2.read_file();
 
     return 0;
 }
