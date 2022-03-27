c0fcc33dccf1bc512642c1a40ec17b55b000896c
Dany-el <daniilyablonskiy88@gmail.com>
2022-03-27 16:28:00 +0300

    test of methods

--- DoneHomework/8.Template/Main.cpp
+++ DoneHomework/8.Template/Main.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <random>
 #include "A_function.h"
-// #include "B_function.h"
-// #include "C_function.h"
+#include "B_function.h"
+#include "C_function.h"
 
-int main() {
+int main()
+{
     srand(time(0));
-    A_function A_temp;
-    int array[3][3];
+    // A
+    // A_function A_temp;
+    // int array[3][3];
+    // A_temp.Init(array,3);
+    // A_temp.Print(array,3);
+    // cout << '\n';
+    // A_temp.Reverse(array,3);
 
-    A_temp.Init(array,3);
-    A_temp.Print(array,3);
-    cout << '\n';
-    A_temp.Reverse(array,3);
- 
+    // B
+    // B_function B_temp;
+    // B_temp.sum(1, 2);
+    // B_temp.diff(3, 2);
+    // B_temp.product(2, 2);
+
+    // C
+    // C_function C_temp;
+
+    // int const size = 10;
+    // int array[size];
+    // for (size_t i = 0; i < size; i++)
+    // {
+    //     array[i] = i + 1;
+    // }
+    
+
+    // C_temp.print_rev_array(array,size);
+    // C_temp.sum_array(array,size);
+    // C_temp.prod_array(array,size);
+    // C_temp.avg_array(array,size);
+    // C_temp.range_array(array,size);
+
     return 0;
 }
